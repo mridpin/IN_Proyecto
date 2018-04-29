e20a3a8eea34a620c855af46e9e17d0b7773f84d
mridpin <mridao99@gmail.com>
2018-04-29 13:40:22 +0200

    terminado el bubblechart

--- CuadroDeMando/ajax/ajax_bubbleChart.php
+++ CuadroDeMando/ajax/ajax_bubbleChart.php
@@ -3,19 +3,31 @@
 ini_set('memory_limit', '1024M');
 // Estructura de $data: [BORO, poblacion, crimenes, crime/population, densidad poblacion] 
 $data = [[]];
-$myfile = fopen("../csv/nyc_pop_by_boro.csv", "r");
-$linea = fgets($myfile);
+$offenseByBoro = [];
+$crimeFile = fopen("../csv/offenseByBoro.csv", "r");
+$linea = fgets($crimeFile);
 $i = 0;
-while ($linea = fgets($myfile)) {
-    $aux = explode(";", $linea);
+// Bucle que guarda un array asociativo de tipo $offenseByBoro["boro"] = # 
+while ($linea = fgets($crimeFile)) {
+    $aux = explode(";", trim($linea));
+    $offenseByBoro[$aux[0]] = $aux[1];
+}
+fclose($crimeFile);
+
+
+$popFile = fopen("../csv/nyc_pop_by_boro.csv", "r");
+$linea = fgets($popFile);
+$i = 0;
+while ($linea = fgets($popFile)) {
+    $aux = explode(";", trim($linea));
     $data[$i] = [];
-    $data[$i][0] = $aux[0];
-    $data[$i][1] = $aux[1];
-    $data[$i][2] = 10000;
-    $data[$i][3] = 10000 / $aux[1];
-    $data[$i][4] = $aux[2];
+    $data[$i][0] = $aux[0]; // Contiene el nombre del boro
+    $data[$i][1] = $aux[1]; // Poblacion total del boro
+    $data[$i][2] = $offenseByBoro[$aux[0]]; // Crimenes totales en el boro
+    $data[$i][3] = $aux[1] / $offenseByBoro[$aux[0]]; // Poblacion / crimenes
+    $data[$i][4] = $aux[2]; // Densidad de poblacion por km2
     $i++;
 }
-fclose($myfile);
+fclose($popFile);
 echo json_encode($data);
 
